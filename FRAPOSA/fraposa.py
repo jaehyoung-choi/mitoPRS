859183fc14aa96df71341376323ae80caced45b4
Jae Choi <123389458+jaehyoung-choi@users.noreply.github.com>
2026-02-12 02:42:45 -0500

    Update modification credits in fraposa.py

--- FRAPOSA/fraposa.py
+++ FRAPOSA/fraposa.py
@@ -1,7 +1,7 @@
 ## Cloned from PGS-Catalog Github (https://github.com/PGScatalog/pgsc_calc), Adapted from FRAPOSA (Original Github: https://github.com/daviddaiweizhang/fraposa)
 ## FRAPOSA: Fast and Robust Ancestry Prediction by Online singular value decomposition and Shrinkage Adjustment
 ## Original Author: David (Daiwei) Zhang
-## Modifications: Samuel Lambert
+## Modifications: Samuel Lambert, Minor Modifications from JC
 
 import matplotlib
 import numpy as np
