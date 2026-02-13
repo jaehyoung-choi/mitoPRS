c816dee0ce1837c1b8ddb1179d905a6d1e12740f
Jae Choi <123389458+jaehyoung-choi@users.noreply.github.com>
2026-02-12 20:38:59 -0500

    Adding bash scripts for scoring

---
+++ run_mitoprs.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+## Input: $1-TargetPrefix $2-OutputPrefix
+## Output: BD-MitoPRS output file in .csv
+
+export target="$1"
+export outpre="$2"
+
+export PATH="$PATH:$HOME/plink1.9/:$HOME/plink2/"
+
+cd "$(dirname "$0")"
+
+## Extracting relevant variants to project PCs using FRAPOSA;
+## Missing Variants will be replaced with ref allele from input REF/ALT format
+
+./align_to_pgc_ref.sh "${target}" "ancestry" "1kgref.bip" #use target, outputprefix, reference bim prefix as arguments
+plink2 --bfile ancestry --extract ref/common_vars_bip.txt --fill-missing-with-ref --make-bed --out "${target}"_tmp #Re-organizing order of variants, forcing to ref of population
+plink2 --bfile "${target}"_tmp --ref-allele 1kgref.bip.bim 6 2 --alt1-allele 5 2 --make-bed --out "${target}"_common #Forcing 1KG REF/ALT format
+
+python -m FRAPOSA.fraposa_runner --stu_filepref "$target"_common --dim_ref 5 --dim_online 20 --out "${target}" 1kgref.bip
+
+mv "${target}".pcs "${target}".oadp
+
+## File Cleanup
+rm -r "${target}"_tmp*
+rm -r "${target}"_common*
+
+./align_to_pgc_ref.sh "${target}" "${target}"_mt "combinedbdset" #$1=TargetPrefix $2=OutPrefix $3=reference bim prefix
+
+Rscript PRSice/PRSice.R \ #Make sure PRSice.R is located in ./PRSice/ directory
+      --prsice PRSice/PRSice_linux \
+      --base ref/BD.mitoPRS.sumstat.postCT.txt \
+      --target "${target}"_mt \
+      --binary-target T \
+      --bar-levels 0.25 \
+      --fast-score \
+      --stat OR \
+      --no-regress --no-clump --no-full \
+      --out "$outpre"
+
+plink --bfile "${target}"_mt --score PRSCSx.bip.combined.txt 2 4 6 --out "$outpre".csx
+
+plink2 --bfile "${target}"_mt --extract ref/xgb_varids.txt --recode A --out "$outpre"_xgb
+awk 'BEGIN {OFS="\t"} {print $1, $2, $3, $4, $5, $6}' "$outpre"_xgb.raw > "outpre"_.pheno
+awk '{for(i=7;i<=NF;i++) printf $i (i==NF?ORS:OFS)}' "$outpre"_xgb.raw | awk 'BEGIN {OFS="\t"} {$1=$1}1' > "$outpre"_xgb.geno
+sed -n '1p' "$outpre"_xgb.geno > varids_xgb.names
+tail -n +2 "$outpre"_xgb.geno > file.tmp && mv file.tmp "$outpre"_xgb.geno
+tail -n +2 "$outpre".pheno | awk '{$1=$1}1' OFS='\t' > file.tmp && mv file.tmp "$outpre".pheno
+
+plink2 --bfile "${target}"_mt --extract ref/enet_varids.txt --recode A --out "$outpre"_enet
+awk '{for(i=7;i<=NF;i++) printf $i (i==NF?ORS:OFS)}' "$outpre"_enet.raw | awk 'BEGIN {OFS="\t"} {$1=$1}1' > "$outpre"_enet.geno
+sed -n '1p' "$outpre"_enet.geno > varids_enet.names
+tail -n +2 "$outpre"_enet.geno > file.tmp && mv file.tmp "$outpre"_enet.geno
+
+python score_mitoprs.py --ext-feature "${outpre}" --ext-cov "${target}".oadp --ext-label "${target}".pheno --train_names "varids" --out-prefix "${outpre}"
+
+#Final Cleanup
+rm *.geno
+rm *.pheno
+rm *.names
+rm *.profile
+rm *.png
+rm *.all_score
+rm *.valid
+rm *.log
+
+
+
+
+
