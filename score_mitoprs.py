a71123e727298ab5ff9e240437bdb53a71c578d0
Jae Choi <123389458+jaehyoung-choi@users.noreply.github.com>
2026-02-18 15:50:21 -0500

    Update score_mitoprs.py

--- score_mitoprs.py
+++ score_mitoprs.py
@@ -12,7 +12,7 @@ from mitoprs_utils import InformedElasticNet
 
 def main():
     # Hardcoded paths as requested
-    XGB_PATH = "model/0.1_xgb_model.pkl"
+    XGB_PATH = "model/0.15_xgb_model.pkl"
     ENET_PATH = "model/0.3_enet_model.pkl"
     
     #Making output directory if doesn't exist
@@ -28,22 +28,17 @@ def main():
     parser.add_argument('--out-prefix', required=True)
     args = parser.parse_args()
 
-    # 1. Load Data
+    # Load Data
     X_new, y_new, full_names, identifiers = utils.load_data_split(
         f"{args.ext_feature}_xgb.geno", args.ext_cov, f"{args.train_names}_xgb.names", args.ext_label
     )
 
-    # 2. Extract FID and IID (First two columns of the .raw file)
-    print(">>> Extracting identifiers...")
-  
-    identifiers.columns = ['FID', 'IID']
-
-    # 3. Load Models
+    # Load Models
     print(f">>> Loading models from /ref/...")
     xgb_model = joblib.load(XGB_PATH)
     enet_model = joblib.load(ENET_PATH)
 
-    # 4. Predict
+    # Predict
     print(">>> Running predictions...")
     # XGBoost
     xgb_probs = xgb_model.predict_proba(X_new)[:, 1]
@@ -56,21 +51,22 @@ def main():
     )
     X_new = X_new.fillna(X_new.median())
     #If median filling still results in NA (e.g. when all values for a given variant is missing, replacing with REF 0/0 genotype)
+    X_new = X_new.fillna(0)
     X_new_np = np.ascontiguousarray(X_new.values, dtype=np.float32)
     
     enet_probs = enet_model.predict_proba(X_new_np)[:, 1]
 
-    # 5. Create Output DataFrame
+    # Create Output DataFrame
     results = identifiers.copy()
-    results.columns = ['FID','IID']
+    results.columns = ['FID', 'IID']
     results['True_Label'] = y_new.values
     results['BD_mitoPRS_XGB'] = xgb_probs
     results['BD_mitoPRS_ENet'] = enet_probs
 
-    # 6. Loading other scores for appending
-    csx_df = pd.read_csv(f"{args.out_prefix}.profile", sep=r'\s+', usecols=['FID', 'IID', 'SCORE']).rename(columns={'SCORE': 'BD_mitoPRS_PRSCSx'})
+    # Loading other scores for appending
+    csx_df = pd.read_csv(f"{args.out_prefix}.csx.profile", sep=r'\s+', usecols=['FID', 'IID', 'SCORE']).rename(columns={'SCORE': 'BD_mitoPRS_PRSCSx'})
     ice_df = pd.read_csv(f"{args.out_prefix}.all_score", sep=r'\s+', usecols=['FID', 'IID', 'Pt_0.25']).rename(columns={'Pt_0.25': 'BD_mitoPRS_PRSice'})
-    pc_df = pd.read_csv(args.ext_cov, usecols=['FID', 'IID', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5'])
+    pc_df = pd.read_csv(args.ext_cov, sep=r'\s+', usecols=['FID', 'IID', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5'])
 
     # 7. Export
     out_file = os.path.join(output_dir, f"{args.out_prefix}_predictions.csv")
@@ -79,10 +75,9 @@ def main():
     results = results.merge(pc_df, on=['FID', 'IID'], how='left')
     results.to_csv(out_file, index=False)
     
-
 if __name__ == "__main__":
-
     main()
+
 
 
 
