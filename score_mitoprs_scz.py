91dd3224d4d856c3db78a799cb1164e7ebbda139
Jae Choi <123389458+jaehyoung-choi@users.noreply.github.com>
2026-04-07 00:37:07 -0400

    Adding bash and py scripts for scz_score
    Adding PRSice, CSx, ENET, XGB for SCZ

---
+++ score_mitoprs_scz.py
@@ -0,0 +1,82 @@
+import joblib
+import os
+import sys
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+from sklearn.metrics import roc_auc_score
+import mitoprs_utils as utils
+sys.modules['enet_utils'] = utils
+# This import is vital: it provides load_data_split_median AND InformedElasticNet
+from mitoprs_utils import InformedElasticNet
+import sklearn
+sklearn.set_config(transform_output="pandas")
+
+def main():
+    # Hardcoded paths as requested
+    XGB_PATH = "model/0.15_xgb_SCZ_model.pkl"
+    ENET_PATH = "model/0.3_scz_enet_model.pkl"
+    
+    #Making output directory if doesn't exist
+    output_dir = "./output"
+    os.makedirs("output", exist_ok=True)
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ext-feature', required=True)
+    parser.add_argument('--ext-cov', required=True)
+    parser.add_argument('--ext-label', required=True)
+    parser.add_argument('--train-names', required=True)
+    parser.add_argument('--out-prefix', required=True)
+    args = parser.parse_args()
+
+    # Load Data
+    X_new, y_new, full_names, identifiers = utils.load_data_split(
+        f"{args.ext_feature}_xgb.geno", args.ext_cov, f"{args.train_names}_xgb.names", args.ext_label
+    )
+
+    # Load Models
+    print(f">>> Loading models from /ref/...")
+    xgb_model = joblib.load(XGB_PATH)
+    enet_model = joblib.load(ENET_PATH)
+
+    # Predict
+    print(">>> Running predictions...")
+    # XGBoost
+    xgb_probs = xgb_model.predict_proba(X_new)[:, 1]
+    
+    del X_new, y_new, full_names, identifiers
+
+    # Elastic Net (using np.float32 for input, filling NA with median of each column)
+    X_new, y_new, full_names, identifiers = utils.load_data_split(
+        f"{args.ext_feature}_enet.geno", args.ext_cov, f"{args.train_names}_enet.names", args.ext_label
+    )
+    X_new.columns = full_names
+    X_new = X_new.fillna(X_new.median())
+    #If median filling still results in NA (e.g. when all values for a given variant is missing, replacing with REF 0/0 genotype)
+    X_new = X_new.fillna(0)
+    X_new_np = np.ascontiguousarray(X_new.values, dtype=np.float32)
+    
+    enet_probs = enet_model.predict_proba(X_new_np)[:, 1]
+
+    # Create Output DataFrame
+    results = identifiers.copy()
+    results.columns = ['FID', 'IID']
+    results['True_Label'] = y_new.values
+    results['SCZ_mitoPRS_XGB'] = xgb_probs
+    results['SCZ_mitoPRS_ENet'] = enet_probs
+
+    # Loading other scores for appending
+    csx_df = pd.read_csv(f"{args.out_prefix}.scz.csx.profile", sep=r'\s+', usecols=['FID', 'IID', 'SCORE']).rename(columns={'SCORE': 'SCZ_mitoPRS_PRSCSx'})
+    ice_df = pd.read_csv(f"{args.out_prefix}.scz.all_score", sep=r'\s+', usecols=['FID', 'IID', 'Pt_0.42']).rename(columns={'Pt_0.42': 'SCZ_mitoPRS_PRSice'})
+    pc_df = pd.read_csv(args.ext_cov, sep=r'\s+', usecols=['FID', 'IID', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5'])
+
+    # 7. Export
+    out_file = os.path.join(output_dir, f"{args.out_prefix}_scz_predictions.csv")
+    results = results.merge(csx_df, on=['FID', 'IID'], how='left')
+    results = results.merge(ice_df, on=['FID', 'IID'], how='left')
+    results = results.merge(pc_df, on=['FID', 'IID'], how='left')
+    results.to_csv(out_file, index=False)
+    
+if __name__ == "__main__":
+    main()
---
+++ score_scz.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+## Input: $1-TargetPrefix $2-OutputPrefix
+## Output: SCZ-MitoPRS output file in .csv
+
+export target="$1"
+export outpre="$2"
+
+export PATH="$PATH:$HOME/plink1.9/:$HOME/plink2/"
+
+cd "$(dirname "$0")"
+
+## Missing Variants will be replaced with ref allele from input REF/ALT format
+
+./align_to_ref.sh "${target}" "${target}"_scz_mt "ref/combinedsczset" #$1=TargetPrefix $2=OutPrefix $3=reference bim prefix
+
+#Make sure PRSice.R is located in ./PRSice/ directory
+#Note summary statistic is already thresholded at p<0.42, and Clumped accordingly to the discovery data structure
+Rscript PRSice/PRSice.R \
+      --prsice PRSice/PRSice_linux \
+      --base ref/SCZ.mitoPRS.sumstat.postCT.txt \
+      --target "${target}"_scz_mt \
+      --binary-target T \
+      --bar-levels 0.42 \
+      --fastscore \
+      --stat OR \
+      --no-regress --no-clump --no-full \
+      --out "$outpre".scz
+
+plink --bfile "${target}"_scz_mt --score ref/PRSCSx.scz.combined.txt 2 4 6 --out "$outpre".scz.csx
+
+# Extracting Variants for Model Input
+
+plink --bfile "${target}"_scz_mt --extract ref/scz_xgb_varids.txt --keep-allele-order --recode A --out "$outpre"_scz_xgb
+
+awk 'BEGIN {OFS="\t"} {print $1, $2, $3, $4, $5, $6}' "$outpre"_scz_xgb.raw > "$outpre".pheno
+awk '{for(i=7;i<=NF;i++) printf $i (i==NF?ORS:OFS)}' "$outpre"_scz_xgb.raw | awk 'BEGIN {OFS="\t"} {$1=$1}1' > "$outpre"_scz_xgb.geno
+sed -n '1p' "$outpre"_scz_xgb.geno > scz_varids_xgb.names
+tail -n +2 "$outpre"_scz_xgb.geno > file.tmp && mv file.tmp "$outpre"_scz_xgb.geno
+tail -n +2 "$outpre".pheno | awk '{$1=$1}1' OFS='\t' > file.tmp && mv file.tmp "$outpre".pheno
+
+plink --bfile "${target}"_scz_mt --extract ref/scz_enet_varids.txt --keep-allele-order --recode A --out "$outpre"_scz_enet
+awk '{for(i=7;i<=NF;i++) printf $i (i==NF?ORS:OFS)}' "$outpre"_scz_enet.raw | awk 'BEGIN {OFS="\t"} {$1=$1}1' > "$outpre"_scz_enet.geno
+sed -n '1p' "$outpre"_scz_enet.geno > varids_enet.names
+tail -n +2 "$outpre"_scz_enet.geno > file.tmp && mv file.tmp "$outpre"_scz_enet.geno
+
+python score_mitoprs_scz.py --ext-feature "${outpre}"_scz --ext-cov "${target}".oadp --ext-label "${outpre}".pheno --train-names "varids" --out-prefix "${outpre}"_scz
+
+#Cleanup for next scoring step
+rm *.geno
+rm *.pheno
+rm varids*
+rm *.profile
+rm *.all_score
+rm *.prsice
+rm *.log
+rm *_model.*
+rm *_mt.*
+rm *.raw
+rm ancestry*
+rm *.names
